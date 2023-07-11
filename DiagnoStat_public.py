66815f1f72244a1eb1581f0c086b7d947da6219b
kalpikha <22.kalpikha@gmail.com>
2023-07-11 18:19:35 +0530

    accuracy improved

--- DiagnoStat_public.py
+++ DiagnoStat_public.py
@@ -1,7 +1,8 @@
 import pickle
 import streamlit as st
+import pandas as pd
 from streamlit_option_menu import option_menu
-import xgboost
+from sklearn.preprocessing import MinMaxScaler
 
 
 # loading the saved models
@@ -156,46 +157,46 @@ if (selected == "Parkinsons Prediction"):
     col1, col2, col3, col4, col5 = st.columns(5)  
     
     with col1:
-        fo = st.text_input('MDVP:Fo(Hz)')
+        fo = st.text_input('MDVP-Fo(Hz)')
         
     with col2:
-        fhi = st.text_input('MDVP:Fhi(Hz)')
+        fhi = st.text_input('MDVP-Fhi(Hz)')
         
     with col3:
-        flo = st.text_input('MDVP:Flo(Hz)')
+        flo = st.text_input('MDVP-Flo(Hz)')
         
     with col4:
-        Jitter_percent = st.text_input('MDVP:Jitter(%)')
+        Jitter_percent = st.text_input('MDVP-Jitter(%)')
         
     with col5:
-        Jitter_Abs = st.text_input('MDVP:Jitter(Abs)')
+        Jitter_Abs = st.text_input('MDVP-Jitter(Abs)')
         
     with col1:
-        RAP = st.text_input('MDVP:RAP')
+        RAP = st.text_input('MDVP-RAP')
         
     with col2:
-        PPQ = st.text_input('MDVP:PPQ')
+        PPQ = st.text_input('MDVP-PPQ')
         
     with col3:
-        DDP = st.text_input('Jitter:DDP')
+        DDP = st.text_input('Jitter-DDP')
         
     with col4:
-        Shimmer = st.text_input('MDVP:Shimmer')
+        Shimmer = st.text_input('MDVP-Shimmer')
         
     with col5:
-        Shimmer_dB = st.text_input('MDVP:Shimmer(dB)')
+        Shimmer_dB = st.text_input('MDVP-Shimmer(dB)')
         
     with col1:
-        APQ3 = st.text_input('Shimmer:APQ3')
+        APQ3 = st.text_input('Shimmer-APQ3')
         
     with col2:
-        APQ5 = st.text_input('Shimmer:APQ5')
+        APQ5 = st.text_input('Shimmer-APQ5')
         
     with col3:
-        APQ = st.text_input('MDVP:APQ')
+        APQ = st.text_input('MDVP-APQ')
         
     with col4:
-        DDA = st.text_input('Shimmer:DDA')
+        DDA = st.text_input('Shimmer-DDA')
         
     with col5:
         NHR = st.text_input('NHR')
@@ -222,11 +223,22 @@ if (selected == "Parkinsons Prediction"):
         PPE = st.text_input('PPE')
         
     
-    
     # code for Prediction
     parkinsons_diagnosis = ''
-    
+
     # creating a button for Prediction    
+    # if st.button("Parkinson's Test Result"):
+    #     data = pd.DataFrame([fo, fhi, flo, Jitter_percent, Jitter_Abs, RAP, PPQ,DDP,Shimmer,Shimmer_dB,APQ3,APQ5,APQ,DDA,NHR,HNR,RPDE,DFA,spread1,spread2,D2,PPE])
+    #     scaler = MinMaxScaler()
+    #     data = pd.DataFrame(scaler.fit_transform(data))
+
+    #     parkinsons_prediction = parkinsons_model.predict(data)                          
+        
+    #     if (parkinsons_prediction[0] == 1):
+    #       parkinsons_diagnosis = "The person has Parkinson's disease"
+    #     else:
+    #       parkinsons_diagnosis = "The person does not have Parkinson's disease"
+
     if st.button("Parkinson's Test Result"):
         parkinsons_prediction = parkinsons_model.predict([[fo, fhi, flo, Jitter_percent, Jitter_Abs, RAP, PPQ,DDP,Shimmer,Shimmer_dB,APQ3,APQ5,APQ,DDA,NHR,HNR,RPDE,DFA,spread1,spread2,D2,PPE]])                          
         
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # DiagnoStat : Multiple Disease Prediction Web App
 
 Multiple Disease Prediction web app for predicting diseases of Heart, Diabetes and Parkinsons disease based on numerical data.
+
